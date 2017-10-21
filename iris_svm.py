f3e1f44577ea30bedcf56a6773dc01039bbf910d
Aidan Fitzgerald <duckjr159@gmail.com>
2017-10-21 11:58:59 -0400

    finish iris demo

--- iris_svm.py
+++ iris_svm.py
@@ -1,6 +1,17 @@
 import numpy as np
 from sklearn import datasets, svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report as clsrpt
+
 
 iris = datasets.load_iris()
+X = iris.data
+y = iris.target
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+svc = svm.LinearSVC()
+svc.fit(X_train, y_train)
+
+y_predict = svc.predict(X_test)
+print(clsrpt(y_test, y_predict))
